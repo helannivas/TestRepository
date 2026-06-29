4d0fab500d4c0d96eb527eb32251c1782b3b1693
helannivas <58122091+helannivas@users.noreply.github.com>
2026-06-29 17:44:45 +0100

    Remove hardcoded user from Snowflake connection
    Removed hardcoded user from Snowflake connection.

--- call_snowflake.py
+++ call_snowflake.py
@@ -57,7 +57,6 @@ print("\n[3/3] Connecting to Snowflake...")
 try:
     conn = snowflake.connector.connect(
         account       = SNOWFLAKE_ACCOUNT,
-        user          = "SSVC_PIPELINE_USER",
         authenticator = "oauth",
         token         = azure_token,
         database      = "SNOWFLAKE_PIPELINE_DB",
